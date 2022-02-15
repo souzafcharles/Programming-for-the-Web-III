753fb6eb6d108716d86a38bdabf9c4033efae9d6
souzafcharles <souzach@outlook.com>
2022-02-15 02:06:23 -0300

    Activity 01 - Website HTML, CSS3 and JavaScript

---
+++ Activity01/js/script.js
@@ -0,0 +1,9 @@
+$('.slide-main').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeigth: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
