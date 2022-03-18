edf8cdd4e777e2009278813fcd52c8989fd1e899
Charles Fernandes de Souza <souzach@outlook.com>
2022-03-17 21:39:56 -0300

    Activity 04 - Servlets Example

--- Activity04/src/main/java/com/souza/charles/activity04/HelloServlet.java
+++ Activity04/src/main/java/com/souza/charles/activity04/HelloServlet.java
@@ -12,11 +12,9 @@ import javax.servlet.annotation.*;
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
-
     public void init() {
         message = "Hello World!";
     }
-
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
@@ -26,7 +24,6 @@ public class HelloServlet extends HttpServlet {
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
     }
-
     public void destroy() {
     }
 }
